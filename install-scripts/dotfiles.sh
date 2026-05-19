18b1cda07e5ab5cb33a2f791f6b81aec64b54ca7
Simon Flink <flinksimon@hotmail.com>
2026-05-19 22:29:57 +0200

    add common.sh and pipefail

--- install-scripts/dotfiles.sh
+++ install-scripts/dotfiles.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SCRIPT_DIR}/common.sh"
 
 mkdir -p "$HOME"/dotfiles
 git clone https://github.com/simfliXD/my-omarchy-dotfiles.git "$HOME"/dotfiles
 
-echo "dotfiles cloned to $HOME/dotfiles."
+info "dotfiles cloned to $HOME/dotfiles."
 
 sudo pacman -S --noconfirm --needed stow
 
@@ -11,4 +15,4 @@ cd "$HOME"/dotfiles
 
 # Stow the dotfiles for the desired applications
 stow *
-echo "dotfiles stowed to $HOME/dotfiles."
+info "dotfiles stowed to $HOME/dotfiles."
